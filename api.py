ced7e944621e029cfe92ab247ef260c413c573bd
gltdevlop <99366409+gltdevlop@users.noreply.github.com>
2025-03-26 11:28:42 +0100

    utf8 pb solved

--- api.py
+++ api.py
@@ -1,29 +1,38 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from scrap_tools.scraper import scrape_data, checking, checking_incor, checking_cor
 from vars import *
 
 app = Flask(__name__)
 
+import json
+from flask import Response
+
 @app.route('/hws', methods=['GET'])
 def scrape_endpoint():
-    result = scrape_data(creds, 'data.txt')
-    return jsonify(result)
+    data = scrape_data(creds, 'data.txt')
+    json_data = json.dumps(data, ensure_ascii=False)
+    return Response(json_data, content_type="application/json; charset=utf-8")
+
 
 @app.route('/checkall', methods=['GET'])
 def check_endpoint():
     result = checking()
-    return jsonify(result)
+    json_data = json.dumps(result, ensure_ascii=False)
+    return Response(json_data, content_type="application/json; charset=utf-8")
+
 
 @app.route('/check_cor', methods=['GET'])
 def check_cor_endpoint():
     result = checking_cor()
-    return jsonify(result)
+    json_data = json.dumps(result, ensure_ascii=False)
+    return Response(json_data, content_type="application/json; charset=utf-8")
 
 
 @app.route('/check_incor', methods=['GET'])
 def check_incor_endpoint():
     result = checking_incor()
-    return jsonify(result)
+    json_data = json.dumps(result, ensure_ascii=False)
+    return Response(json_data, content_type="application/json; charset=utf-8")
 
 
 if __name__ == '__main__':
